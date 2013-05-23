bb6edb26ba33403abcf35b8dc9802967d25e219b
Michael Jørgen Wilsted Thomsen <mjwt@live.dk>
2013-05-23 12:20:03 +0200

    get mail

--- MerMail/MerMail/Program.cs
+++ MerMail/MerMail/Program.cs
@@ -17,7 +17,29 @@ namespace MerMail
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
+            //Application.Run(new ); //later
             Application.Run(new Form1());
+        }
+        public static List<OpenPop.Mime.Message> FetchAllMessages(string Hostname, int port, bool usessl, string username, string password)
+        {
+            using(OpenPop.Pop3.Pop3Client client = new OpenPop.Pop3.Pop3Client())
+            {
+                //connection
+                client.Connect(Hostname, port, usessl);
+
+                // authenticate
+                client.Authenticate(username, password);
+
+                int messageCount = client.GetMessageCount();
+
+                List<OpenPop.Mime.Message> allMessages = new List<OpenPop.Mime.Message>(messageCount);
+
+                for (int i = messageCount; i > 0; i--)
+                {
+                    allMessages.Add(client.GetMessage(i));
+                }
+                return allMessages;
+            }
         }
     }
 }
