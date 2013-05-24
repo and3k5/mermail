77ab07a5fc7d6b0a85dd8e38423d36fcde70006f
Anders K <ande7353@gmail.com>
2013-05-24 10:20:29 +0200

    insertUser sql function

--- MerMail/MerMail/Program.cs
+++ MerMail/MerMail/Program.cs
@@ -21,8 +21,11 @@ namespace MerMail
             //Application.Run(new ); //later
             initMermailDB();
             Application.Run(new Login());
+            MessageBox.Show("Nu lukkes Loginform - sqlCon afbrydes");
+            sqlCon.Close();
         }
         public readonly static string appdata = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+        public static SQLiteConnection sqlCon;
         public static void initMermailDB()
         {
             if (System.IO.Directory.Exists(appdata+"/mermail/") == false)
@@ -33,19 +36,44 @@ namespace MerMail
             conStr.DataSource = appdata+"/mermail/mermail.db";
             conStr.Version = 3;
 
-            SQLiteConnection sqlCon = new SQLiteConnection(conStr.ConnectionString);
+            sqlCon = new SQLiteConnection(conStr.ConnectionString);
 
             sqlCon.Open();
 
             SQLiteCommand cmd = sqlCon.CreateCommand();
             cmd.CommandText = "CREATE TABLE IF NOT EXISTS users ( id INTEGER PRIMARY KEY AUTOINCREMENT, mailaddress VARCHAR(255) NOT NULL, password VARCHAR(255) NOT NULL, pop_hostname VARCHAR(255) not null, pop_port INTEGER not null, pop_ssl BOOLEAN not null )";
             cmd.ExecuteNonQuery();
-            sqlCon.Close();
+            //sqlCon.Close();
             //con.CreateFile("%appdata%/.mermail/mermail.db");
 
 
 
 
+        }
+        public static int insertUser(string mailaddr, string password, string pop_hostname, int pop_port, bool pop_ssl)
+        {
+            //SQLiteTransaction transact;
+            string sqlquery = "INSERT INTO users (mailaddress,password,pop_hostname,pop_port,pop_ssl) ";
+            sqlquery += "VALUES (@_mailaddress,@_password,@_pop_hostname,@_pop_port,@_pop_ssl)";
+
+            SQLiteCommand cmd = sqlCon.CreateCommand();
+            cmd.CommandText = sqlquery;
+            cmd.Parameters.AddWithValue("@_mailaddress", mailaddr);
+            cmd.Parameters.AddWithValue("@_password", password);
+            cmd.Parameters.AddWithValue("@_pop_hostname", pop_hostname);
+            cmd.Parameters.AddWithValue("@_pop_port", pop_port);
+            int popSSL = 0;
+            if (pop_ssl == true) popSSL = 1;
+
+            cmd.Parameters.AddWithValue("@_pop_ssl", popSSL);
+
+            //transact = sqlCon.BeginTransaction();
+            int rtn=cmd.ExecuteNonQuery();
+
+            return 0;
+            
+            //cmd.CommandText = "CREATE TABLE IF NOT EXISTS users ( id INTEGER PRIMARY KEY AUTOINCREMENT, mailaddress VARCHAR(255) NOT NULL, password VARCHAR(255) NOT NULL, pop_hostname VARCHAR(255) not null, pop_port INTEGER not null, pop_ssl BOOLEAN not null )";
+            //cmd.ExecuteNonQuery();
         }
         public static List<OpenPop.Mime.Message> FetchAllMessages(string Hostname, int port, bool usessl, string username, string password)
         {
