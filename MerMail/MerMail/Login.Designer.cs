207e779ad9453a6845a1db5ca511f842ed6bbcf6
Michael Jørgen Wilsted Thomsen <mjwt@live.dk>
2013-05-28 14:03:17 +0200

    change login

--- MerMail/MerMail/Login.Designer.cs
+++ MerMail/MerMail/Login.Designer.cs
@@ -39,13 +39,17 @@
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.SmtpName = new System.Windows.Forms.TextBox();
+            this.SmtpPort = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // loginBtn
             // 
-            this.loginBtn.Location = new System.Drawing.Point(86, 161);
+            this.loginBtn.Location = new System.Drawing.Point(86, 212);
             this.loginBtn.Name = "loginBtn";
-            this.loginBtn.Size = new System.Drawing.Size(89, 19);
+            this.loginBtn.Size = new System.Drawing.Size(89, 25);
             this.loginBtn.TabIndex = 10;
             this.loginBtn.Text = "Log in";
             this.loginBtn.UseVisualStyleBackColor = true;
@@ -53,7 +57,7 @@
             // 
             // PassBox
             // 
-            this.PassBox.Location = new System.Drawing.Point(75, 108);
+            this.PassBox.Location = new System.Drawing.Point(75, 159);
             this.PassBox.Name = "PassBox";
             this.PassBox.PasswordChar = '*';
             this.PassBox.Size = new System.Drawing.Size(100, 20);
@@ -61,7 +65,7 @@
             // 
             // UserBox
             // 
-            this.UserBox.Location = new System.Drawing.Point(75, 82);
+            this.UserBox.Location = new System.Drawing.Point(75, 133);
             this.UserBox.Name = "UserBox";
             this.UserBox.Size = new System.Drawing.Size(100, 20);
             this.UserBox.TabIndex = 8;
@@ -69,7 +73,7 @@
             // useSslCheckbox
             // 
             this.useSslCheckbox.AutoSize = true;
-            this.useSslCheckbox.Location = new System.Drawing.Point(75, 59);
+            this.useSslCheckbox.Location = new System.Drawing.Point(75, 110);
             this.useSslCheckbox.Name = "useSslCheckbox";
             this.useSslCheckbox.Size = new System.Drawing.Size(68, 17);
             this.useSslCheckbox.TabIndex = 13;
@@ -102,7 +106,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 35);
+            this.label2.Location = new System.Drawing.Point(24, 32);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(48, 13);
             this.label2.TabIndex = 15;
@@ -111,7 +115,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(12, 85);
+            this.label4.Location = new System.Drawing.Point(12, 136);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(60, 13);
             this.label4.TabIndex = 17;
@@ -120,7 +124,7 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(12, 111);
+            this.label5.Location = new System.Drawing.Point(12, 162);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(56, 13);
             this.label5.TabIndex = 18;
@@ -129,17 +133,53 @@
             // comboBox1
             // 
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(54, 134);
+            this.comboBox1.Location = new System.Drawing.Point(54, 185);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(121, 21);
             this.comboBox1.TabIndex = 19;
             this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
             // 
+            // SmtpName
+            // 
+            this.SmtpName.Location = new System.Drawing.Point(75, 58);
+            this.SmtpName.Name = "SmtpName";
+            this.SmtpName.Size = new System.Drawing.Size(100, 20);
+            this.SmtpName.TabIndex = 20;
+            // 
+            // SmtpPort
+            // 
+            this.SmtpPort.Location = new System.Drawing.Point(75, 84);
+            this.SmtpPort.Name = "SmtpPort";
+            this.SmtpPort.Size = new System.Drawing.Size(100, 20);
+            this.SmtpPort.TabIndex = 21;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 58);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(63, 13);
+            this.label3.TabIndex = 22;
+            this.label3.Text = "Smtpserver:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(16, 84);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(56, 13);
+            this.label6.TabIndex = 23;
+            this.label6.Text = "Smtp Port:";
+            // 
             // Login
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(185, 190);
+            this.ClientSize = new System.Drawing.Size(185, 241);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.SmtpPort);
+            this.Controls.Add(this.SmtpName);
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
@@ -151,6 +191,7 @@
             this.Controls.Add(this.loginBtn);
             this.Controls.Add(this.PassBox);
             this.Controls.Add(this.UserBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.Name = "Login";
             this.Text = "Login";
             this.Load += new System.EventHandler(this.Login_Load);
@@ -172,6 +213,10 @@
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox SmtpName;
+        private System.Windows.Forms.TextBox SmtpPort;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label6;
 
     }
 }
