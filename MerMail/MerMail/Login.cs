23bfad1c33129205c54d2d27d843c4ebbb539355
Michael Jørgen Wilsted Thomsen <mjwt@live.dk>
2013-06-02 14:41:26 +0200

    cmments

--- MerMail/MerMail/Login.cs
+++ MerMail/MerMail/Login.cs
@@ -12,6 +12,7 @@ namespace MerMail
 {
     public partial class Login : Form
     {
+        //Makes the tooltips
         ToolTip PopHostNTP = new ToolTip();
         ToolTip PopPortTP = new ToolTip();
         ToolTip PopSSLTP = new ToolTip();
@@ -25,6 +26,8 @@ namespace MerMail
         public Login()
         {
             InitializeComponent();
+            #region tooltips
+            //Makes tooltips visible and defines what is shown in the tooltip
             PopHostNTP.ShowAlways = true;
             PopPortTP.ShowAlways = true;
             PopSSLTP.ShowAlways = true;
@@ -45,28 +48,36 @@ namespace MerMail
             PassTP.SetToolTip(PassBox, "Password");
             ComboTP.SetToolTip(comboBox1, "Previus users on this pc");
             loginbtnTP.SetToolTip(loginBtn, "Login");
+            #endregion
         }
         private List<MerMail.Program.mailaccount> accounts;
         private void Login_Load(object sender, EventArgs e)
         {
-            this.BringToFront();
+            //makes shortcut to call a function from program.cs
             accounts = MerMail.Program.getAccounts();
+            
+            //clear the combobox
             comboBox1.Items.Clear();
+            
+            //adds the users to the combobox
             foreach (MerMail.Program.mailaccount acc in accounts)
             {
                 comboBox1.Items.Add(acc.mailaddress);
             }
-
         }
 
         private void loginBtn_Click(object sender, EventArgs e)
         {
             try
             {
+                //checks if the user can be connectec with to the mailserver
                 bool auth = MerMail.Program.AuthenticateLog(HostNameBox.Text, Convert.ToInt32(PortBox.Text), useSslCheckbox.Checked, UserBox.Text, PassBox.Text);
                 if (auth)
                 {
+                    //if it connects add it to the databse over users
                     MerMail.Program.insertUser(UserBox.Text, PassBox.Text, HostNameBox.Text, Convert.ToInt16(PortBox.Text), useSslCheckbox.Checked,SmtpName.Text,Convert.ToInt16(SmtpPort.Text),usesmtpssl.Checked);
+                    
+                    //changes the dialogresult
                     this.DialogResult = DialogResult.OK;
                 }
                 else
@@ -82,6 +93,7 @@ namespace MerMail
 
         private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
+            //when you change user from the combobox then it fills in the data to the fields (username, hostname, etc..)
             MerMail.Program.mailaccount selectedAccount = accounts.ToArray()[(sender as ComboBox).SelectedIndex];
             HostNameBox.Text = selectedAccount.pop_hostname;
             PortBox.Text = selectedAccount.pop_port.ToString();
--- MerMail/MerMail/Send.cs
+++ MerMail/MerMail/Send.cs
@@ -12,6 +12,7 @@ namespace MerMail
 {
     public partial class Send : Form
     {
+        //makes the tooltips
         ToolTip TBtip = new ToolTip();
         ToolTip SubjectTip = new ToolTip();
         ToolTip MailTip = new ToolTip();
@@ -20,23 +21,24 @@ namespace MerMail
         {
             InitializeComponent();
             
+            //show and injects the data
             TBtip.ShowAlways = true;
             TBtip.SetToolTip(TB, "Send mail To");
-            
             SubjectTip.ShowAlways = true;
             SubjectTip.SetToolTip(Subjectbox, "What is the subject");
-            
             MailTip.ShowAlways = true;
             MailTip.SetToolTip(BodyBox, "Type Your Mail here");
         }
 
         private void Sendbtn_Click(object sender, EventArgs e)
         {
+            //Makes the mail
             MerMail.Program.email mail = new MerMail.Program.email(TB.Text, Subjectbox.Text, BodyBox.Text);
             if (useEncryption.Checked)
             {
                 try
                 {
+                    //encypts the body id checkbox is checked
                     mail = MerMail.Program.encryptMail(mail, useSymmetric, SymmetricKey, useAsymmetric, public_key);
                 }
                 catch (Exception err)
@@ -44,10 +46,8 @@ namespace MerMail
                     MessageBox.Show("Fejl ved encryptering:" + Environment.NewLine + err.Message);
                     return;
                 }
-                //decForm.private_key
-                //decForm.private
             }
-            
+            //sends the mail
             MerMail.Program.SendMail(mail.to, mail.subject, mail.body);
             this.Close();
         }
@@ -61,14 +61,14 @@ namespace MerMail
         {
             encSetBtn.Enabled = useEncryption.Checked;
         }
+        //sets some public/global values
         public bool useSymmetric = false;
         public bool useAsymmetric = false;
         public MerMail.Asymmetric.Key public_key = new MerMail.Asymmetric.Key();
         public string SymmetricKey = "";
-
-
         private void encSetBtn_Click(object sender, EventArgs e)
         {
+            //shows the encryption form
             encryptForm encForm = new encryptForm();
             if (encForm.ShowDialog() == DialogResult.OK)
             {
